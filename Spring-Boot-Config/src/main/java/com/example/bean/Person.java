b92a8c0ea63feda4ad4f065d4c54ffb74bc8e131
Taiji-lizheng <48743962+Taiji-lizheng@users.noreply.github.com>
2019-04-01 16:28:07 +0800

    SpringBoot关于Bean类，通过yml，properties， @value等方式赋值

---
+++ Spring-Boot-Config/src/main/java/com/example/bean/Person.java
@@ -0,0 +1,31 @@
+package com.example.bean;
+
+import java.io.Serializable;
+
+
+public class Person implements Serializable {
+	private static final long serialVersionUID = 4464233896798170487L;
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+}
+
+
+// https://projectlombok.org/
